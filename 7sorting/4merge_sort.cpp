8fd55920dd897126d0e21f286041ab9174d1c5f2
Priyanshuvts <priyanshuvatz@gmail.com>
2026-02-26 16:43:16 +0530

    merge sort

--- 7sorting/4merge_sort.cpp
+++ 7sorting/4merge_sort.cpp
@@ -1,14 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-void ms(int arr[], int st , int ed){
-    if(st >= ed) return;
-    int mid = ed + (st - ed)/2;
-    ms(arr,st,mid);
-    ms(arr,mid + 1, ed);
+void merge(vector <int> &v , int st, int mid, int ed){
+    vector <int> temp ;
+    int l = st, r = mid + 1;
+    while(l <= mid && r <= ed){
+        if(v[l] < v[r]){
+            temp.push_back(v[l]);
+            l ++;
+        }
+        else{
+            temp.push_back(v[r]);
+            r++;
+        }
+    }
+    while(l <= mid){
+        temp.push_back(v[l]);
+        l ++;
+    }
+    while(r <= ed){
+        temp.push_back(v[r]);
+        r++;
+    }
+    for(auto val : temp){
+        v[st] = val;
+        st ++;
+    }
+
+}
+void ms(vector <int> &v, int st, int ed){
+    if (st >= ed) return;
+    int mid = (ed + st)/2;
+    ms(v , st , mid);
+    ms(v , mid + 1, ed);
+    merge(v, st , mid , ed);
+
 }
 int main() {
-    int arr[] = {3,2,4,1,3};
-    int n = sizeof(arr)/4;
-    
+    vector<int> v = {3,4,32,5,6,4,89,97,7};
+    int n= v.size();
+    ms(v,0,n-1);
+    for(auto vl: v) cout << vl << " ";
     return 0;
 }
